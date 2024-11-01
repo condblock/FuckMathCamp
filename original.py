23f094be1db66e1b2827001dadac8590eccce60e
COND_B <72214833+condblock@users.noreply.github.com>
2024-11-01 19:54:19 +0900

    init1

--- original.py
+++ original.py
@@ -12,7 +12,8 @@ def approx(f, a, b, n):
 if __name__=='__main__':
     start = time.time()
     def f(x): return x**3  #함수 입력
-    n_values=[2, 4, 6, 8, 10, 20, 40, 80]  #구간 개수 입력
+    n_values=[2, 4, 6, 8, 10, 20, 40, 80] #구간 개수 입력
+    #n_values=range(1, 10**3)
     a=1.0  #적분 구간 아래 끝
     b=2.0  #적분 구간 위 끝
     Int=15.0/4  #미적분의 기본 정리로 계산한 참값
